2eacbb3c564cfeaa9a165fd95bdf3ac2300e1cfd
chewra-wm23-crypto <chewra-wm23@student.tarc.edu.my>
2025-08-24 22:03:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee1.ccxxwexi7thf.us-east-1.rds.amazonaws.com"
+customhost = "employee.ccxxwexi7thf.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
